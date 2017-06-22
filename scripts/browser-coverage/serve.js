45257d7a7e5dd001fb8d8a606228c1573b2457ac
Geoff Cox <redgeoff@gmail.com>
2017-06-22 06:55:14 -0700

    refactor(browser-coverage): move logic to server

---
+++ scripts/browser-coverage/serve.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var argv = require('minimist')(process.argv.slice(2)),
+  Server = require('./server');
+
+if (!argv.c || !argv.t) {
+  console.log('Usage: concat -c cache-dir -t test-js-file [ -p port ]');
+  process.exit(-1);
+}
+
+var server = new Server(argv.c, argv.t, argv.p);
+
+server.serve().catch(function (err) {
+  console.error(err);
+  process.exit(-1);
+});
--- scripts/browser-coverage/server.js
+++ scripts/browser-coverage/server.js
@@ -1,73 +1,101 @@
-#!/usr/bin/env node
-
-// TODO: refactor into OOP and bin layer
 // TODO: should be able to reuse a lot of logic between browser and browser-coverage
 
 'use strict';
 
 var path = require('path'),
-  argv = require('minimist')(process.argv.slice(2));
+  istanbul = require('browserify-istanbul'),
+  mkdirp = require('mkdirp-promise'),
+  utils = require('../utils'),
+  browserify = require('browserify'),
+  http = require('http'),
+  express = require('express'),
+  Promise = require('bluebird');
 
-var istanbul = require('browserify-istanbul');
-var path = require('path');
-var mkdirp = require('mkdirp-promise');
-var fs = require('fs');
-var utils = require('../utils');
-var browserify = require('browserify');
-var http = require('http');
-var express = require('express');
+var Server = function (cacheDir, testJsFile, port) {
+  this._cacheDir = cacheDir;
+  this._testJsFile = testJsFile;
+  this._port = port ? port : Server.DEFAULT_PORT;
 
-if (!argv.c || !argv.t) {
-  console.log('Usage: concat -c cache-dir -t test-js-file [ -p port ]');
-  process.exit(-1);
-}
+  this._app = express();
+  this._htmlDir = path.join(this._cacheDir, 'browser-coverage');
+  this._dotFile = this._htmlDir + '/.bundle.js';
+  this._outFile = this._htmlDir + '/bundle.js';
+};
 
-var HTTP_PORT = argv.p ? argv.p : 8001;
-var htmlDir = path.join(argv.c, 'browser-coverage');
-var indexfile = argv.t;
-var dotFile = htmlDir + '/.bundle.js';
-var outFile = htmlDir + '/bundle.js';
-var app = express();
-var utils = require('../utils');
+Server.DEFAULT_PORT = 8001;
 
-var b = browserify(indexfile, {
-  cache: {},
-  packageCache: {},
-  fullPaths: true,
-  debug: true
-});
-
-b.transform(istanbul({
-  ignore: ['**/node_modules/**']
-}));
-
-var files = [{
-    src: path.join(__dirname, 'index.html'),
-    dst: path.join(htmlDir, 'index.html')
-  },
-  {
-    src: path.join(__dirname, '../../node_modules/mocha/mocha.css'),
-    dst: path.join(htmlDir, 'mocha.css')
-  },
-  {
-    src: path.join(__dirname, '../../node_modules/mocha/mocha.js'),
-    dst: path.join(htmlDir, 'mocha.js')
-  },
-  {
-    src: path.join(__dirname, '../../node_modules/chai/chai.js'),
-    dst: path.join(htmlDir, 'chai.js')
-  }
-];
-
-mkdirp(htmlDir).then(function () {
-  // We need to copy files to the cache so that we can expose a single directory to our web server
-  return utils.copyFiles(files);
-}).then(function () {
-  return utils.concat(b, dotFile, outFile);
-}).then(function () {
-  app.use(express.static(argv.c));
-  var server = http.createServer(app);
-  server.listen(HTTP_PORT, function () {
-    console.log('Tests: http://127.0.0.1:' + HTTP_PORT + '/browser-coverage/index.html');
+Server.prototype._concat = function () {
+  this._b = browserify(this._testJsFile, {
+    cache: {},
+    packageCache: {},
+    fullPaths: true,
+    debug: true
   });
-});
+
+  this._b.transform(istanbul({
+    ignore: ['**/node_modules/**']
+  }));
+
+  return utils.concat(this._b, this._dotFile, this._outFile);
+};
+
+Server.prototype._copyFiles = function () {
+
+  // We need to copy files to the cache so that we can expose a single directory to our web server
+
+  return utils.copyFiles([{
+      src: path.join(__dirname, 'index.html'),
+      dst: path.join(this._htmlDir, 'index.html')
+    },
+    {
+      src: path.join(__dirname, '../../node_modules/mocha/mocha.css'),
+      dst: path.join(this._htmlDir, 'mocha.css')
+    },
+    {
+      src: path.join(__dirname, '../../node_modules/mocha/mocha.js'),
+      dst: path.join(this._htmlDir, 'mocha.js')
+    },
+    {
+      src: path.join(__dirname, '../../node_modules/chai/chai.js'),
+      dst: path.join(this._htmlDir, 'chai.js')
+    }
+  ]);
+
+};
+
+Server.prototype._createHTMLDir = function () {
+  return mkdirp(this._htmlDir);
+};
+
+Server.prototype._createServer = function () {
+  this._app.use(express.static(this._cacheDir));
+  this._server = http.createServer(this._app);
+};
+
+Server.prototype._listen = function () {
+  var self = this;
+  return new Promise(function (resolve) {
+    self._server.listen(self._port, resolve);
+  });
+};
+
+Server.prototype._createServerAndListen = function () {
+  this._createServer();
+  return this._listen();
+};
+
+Server.prototype.serve = function () {
+  var self = this;
+
+  return self._createHTMLDir().then(function () {
+    return self._copyFiles();
+  }).then(function () {
+    return self._concat();
+  }).then(function () {
+    return self._createServerAndListen();
+  }).then(function () {
+    console.log('Tests: http://127.0.0.1:' + self._port + '/browser-coverage/index.html');
+  });
+};
+
+module.exports = Server;
